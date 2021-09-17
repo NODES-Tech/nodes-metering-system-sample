dc8809666a240ea3b25df3d697ef9e19b2cd9cc0
Kai Drange <kaimdrange@hotmail.com>
2021-09-17 09:22:58 +0200

    Add pseudo code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Nodes.MeteringSystem.Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DSO example:
+            
+            //set ut authorization
+            // Get asset grid assignments
+            // create meter reading sample values
+            // Save to NODES
+            // Get data as CSV
+            // Delete data
+            
+            // FSP example:
+            //set ut authorization
+            // Get assets
+            // Get portfolios, check that aggregation is on
+            // create meter reading sample values
+            // Save to NODES
+            // Get data as CSV
+            // Delete data
         }
     }
 }
