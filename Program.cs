ff364dedb136284885458bfb5122c6fe34884b0d
Kai Drange <kaimdrange@hotmail.com>
2021-09-17 09:16:52 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nodes.MeteringSystem.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
