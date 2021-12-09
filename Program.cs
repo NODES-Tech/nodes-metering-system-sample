00931e03a26e5790741fd421d8f44bbc0dec1ffe
Kai Drange <kai.drange@ae.no>
2021-12-09 12:54:54 +0100

    Add more comments to example

--- Program.cs
+++ Program.cs
@@ -19,57 +19,77 @@ namespace Nodes.MeteringSystem.Sample
         private static async Task Main(string[] args)
         {
             await new Program().Run();
-            
-            // FSP example:
-            //set ut authorization
-            // Get assets
-            // Get portfolios, check that aggregation is on
-            // create meter reading sample values
-            // Save to NODES
-            // Get data as CSV
-            // Delete data
         }
 
         private async Task Run()
         {
+            // This example illustrates how a DSO can upload/search for/delete meter readings for
+            // AssetGridAssignments using the Nodes.API.Http.Client nuget package to integrate with NODES.
+            
             var client = _services.GetRequiredService<NodesClient>();
             
-            //Example 1: DSO Uploads meter readings for AssetGridAssignments, gets them as CSV, and finally deletes them.
+            // Requirement:
+            // For a DSO to be allowed to provide meter readings for an asset grid assignment, the FSP
+            // need to have those assets organized in portfolios where NODES is set as Meter Readings source
+            // Also, "Aggregate meter readings from assets" checked on the portfolio(s) - i.e. the FSP has configured
+            // the portfolio(s) to accept meter readings on the individual assets/asset grid assignments and let
+            // NODES aggregate those values up to portfolio level.
             var assetGridAssignmentsToUse = new[]
             {
-                "225b356b-e953-4e06-a953-81533dfbd800",
-                "3c9e5053-c997-4bc5-8ec0-da876a792bad"
+                "636b466f-6c15-449f-859b-7c1971c0735f"
             };
-            
+
+            // Search for the assetGridAssignments with the IDs provided
             var agaSearchFilters = new List<KeyValuePair<string, IFilter>>
             {
                 KeyValuePair.Create(nameof(AssetGridAssignment.Id), OneOfMatcher.OneOf(assetGridAssignmentsToUse))
             };
-            
             var assetGridAssignments = (await client.AssetGridAssignments.Search(agaSearchFilters)).Items;
+
+            // Create some "dummy" meter readings to have some data to insert
             var start = new DateTimeOffset(DateTimeOffset.Now.Year, DateTimeOffset.Now.Month, DateTimeOffset.Now.Day, 0,
                 0, 0, 0, TimeSpan.Zero);
             var end = start.AddHours(1);
             var meterReadings = CreateExampleData(assetGridAssignments, start, end).ToList();
 
-            // Insert new meter readings
+            // Insert the new meter readings. This can be for one or for multiple asset grid assignments. If there already is
+            // existing data for those IDs, validation will fail with an error message. In other words, existing data will not
+            // be replaced. If overwriting data is the intent, the old meter readings must first be deleted.
             await client.MeterReadings.CreateMultiple(meterReadings);
 
-            // Search for existing meter readings for these ids and period
+            // Create set of search filters to search for the newly inserted data
             var mrSearchFilters = new List<KeyValuePair<string, IFilter>>
             {
                 KeyValuePair.Create(nameof(MeterReading.AssetGridAssignmentId), OneOfMatcher.OneOf(assetGridAssignmentsToUse)),
                 KeyValuePair.Create(nameof(MeterReading.PeriodFrom), new DateTimeRange { StartOnOrAfter = start } as IFilter),
                 KeyValuePair.Create(nameof(MeterReading.PeriodTo), new DateTimeRange { EndBefore = end.AddMinutes(10) } as IFilter)
-                // KeyValuePair.Create(nameof(MeterReading.PeriodFrom), DateTimeRange.InRange(start, end.AddSeconds(1)))
             };
-            var foundMeterReadings = await client.MeterReadings.Search(mrSearchFilters, SearchOptions.Max);
+
+            // ...then search for the data using the searchFilter. Search results in NODES will have a max amount of hits. Even though
+            // it is not needed for the small amount of data in this example, using Take and Skip to do multiple searches as shown below
+            // is recommended to be sure that ALL items matching the search is fetched.
+            const int take = 100;
+            var skip = 0;
+            SearchResult<MeterReading> searchResult;
+            var meterReadingsFound = new List<MeterReading>();
+            do
+            {
+                searchResult = await client.MeterReadings.Search(mrSearchFilters, new SearchOptions
+                {
+                    Take = take,
+                    Skip = skip,
+                    OrderBy = { "PeriodFrom" }
+                });
+                meterReadingsFound.AddRange(searchResult.Items);
+                skip += take;
+            } while (searchResult.Items.Count > 0 && searchResult.Items.Count >= take);
+
 
             // Delete meter readings for these ids using the same search filter
             var deletedItemCount = await client.MeterReadings.Delete(mrSearchFilters);
         }
 
-        private IEnumerable<MeterReading> CreateExampleData(
+        private static IEnumerable<MeterReading> CreateExampleData(
             IReadOnlyCollection<AssetGridAssignment> assetGridAssignments,
             DateTimeOffset from,
             DateTimeOffset to)
